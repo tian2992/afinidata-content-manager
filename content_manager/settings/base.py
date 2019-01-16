bf0532cc98c5b92712b2e7dad4d6b55116ca45fb
Alejandro Reyna <AleejandroReyna@gmail.com>
2019-01-16 13:01:55 -0600

    configuration for mysql conector

--- content_manager/settings/base.py
+++ content_manager/settings/base.py
@@ -11,6 +11,16 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
