91ca1843c2498699819a66173f9d399f86dd9964
Alejandro Reyna <AleejandroReyna@gmail.com>
2019-01-16 10:06:25 -0600

    Try to set production project (added settings folder, added base.py, added local and production enviroment, change uri in manage.py, change variable in wsgi.py)

--- content_manager/settings/base.py
+++ content_manager/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h+$+@guqjpn=8q*_-&sf(s4eri$)3@4#&yn9u3tgfkxkxht9m2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
