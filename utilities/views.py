dab32af283b35ccd48f909fa7cde995916e3baeb
Alejandro Reyna <AleejandroReyna@gmail.com>
2019-01-24 14:07:00 -0600

    Update valid date request

--- utilities/views.py
+++ utilities/views.py
@@ -6,11 +6,14 @@ import re
 @csrf_exempt
 def validates_date(request):
 
+    print(request)
+
     if request.method == 'GET':
         try:
             if request.GET['date']:
                 result = re.match("[\d]{1,2}/[\d]{1,2}/[\d]{4}", request.GET['date'])
-                if result:
+                second_result = re.match("[\d]{1,2}%2F[\d]{1,2}%2F[\d]{4}", request.GET['date'])
+                if result or second_result:
                     return JsonResponse(dict(isDateValid=True))
                 else:
                     return JsonResponse(dict(isDateValid=False))
