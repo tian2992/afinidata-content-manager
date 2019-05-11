11f4885731ab08fde13b0bfa5ca97904bd8841e9
Alejandro Reyna <AleejandroReyna@gmail.com>
2019-05-10 18:41:29 -0600

    unit test added

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from posts.models import Post
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class PostTestCase(TestCase):
+
+    def setUp(self):
+        user = User.objects.create(username='testuser', password='123456')
+        Post.objects.create(name='test 01', thumbnail='adsfasdf', new=True, min_range=5, max_range=20,
+                            content='adsfadsf', content_activity='asdfasdf', user=user)
+        Post.objects.create(name='test 02', thumbnail='adsfasdf', new=True,
+                            content='adsfadsf', content_activity='asdfasdf', user=user)
+
+    def post_has_created(self):
+        post = Post.objects.get(name='test 01')
+        second_post = Post.objects.get(name='test 02')
+        self.assertEqual(post.new, True)
+        #self.assertNotEqual(second_post.new, True)
+
