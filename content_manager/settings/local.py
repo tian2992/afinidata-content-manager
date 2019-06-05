c7317e612baefb57a1383c88745a5a0a741d031b
Sebastian Oliva <code@sebastianoliva.com>
2019-06-05 15:49:37 -0600

    local settings w/ localdb

--- content_manager/settings/local.py
+++ content_manager/settings/local.py
@@ -3,3 +3,16 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'default',
+    },
+    'messenger_users_db': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'messengeruser',
+    }
+}
+    
