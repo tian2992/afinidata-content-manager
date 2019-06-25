f475e29a451dd009a33d0a8bad0524bfa2f085f0
Sebastian Oliva <code@sebastianoliva.com>
2019-06-25 16:52:34 -0600

    errolo

--- posts/views.py
+++ posts/views.py
@@ -1034,9 +1034,9 @@ def set_interaction_to_post(request):
         post_id = request.POST['post_id']
         post=Post.objects.get(id=post_id)
     except Exception as e:
+        logger.error(request)
         logger.error("error when setting interaction post_id needed or invalid")
         logger.error(e)
-        logger.info(request)
         post = None
         return JsonResponse(dict(status='error', error='post_id is needed param.'))
 
