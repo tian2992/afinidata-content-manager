09ae75378097a13e05ea76caac30b15b46cd00ee
Sebastian Oliva <code@sebastianoliva.com>
2019-07-17 17:30:09 -0600

    add timez

--- messenger_users/views.py
+++ messenger_users/views.py
@@ -140,7 +140,7 @@ def add_attribute(request, channel_id):
 def last_interacted(request, id=None):
     def dictfetchall(cursor):
         "Return all rows from a cursor as a dict"
-        columns = [col[0] for col in cursor.description]
+        columns = [col[0]+'_time' for col in cursor.description]
         return [
             dict(zip(columns, row))
             for row in cursor.fetchall()
