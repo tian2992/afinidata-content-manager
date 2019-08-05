b1e3a01cf5c7c37ada8b38479f5a10db723ea2c2
Sebastian Oliva <code@sebastianoliva.com>
2019-08-05 12:54:14 -0600

    no curser

--- messenger_users/urls.py
+++ messenger_users/urls.py
@@ -5,7 +5,6 @@ app_name = 'messenger_users'
 
 urlpatterns = [
     path('new/', new_user, name="new"),
-    path('chatfuel_user', cf_user, name="cuser"),
     path('<channel_id>/add_attribute/', add_attribute, name="add_attribute"),
     path('by_username/<username>/', by_username, name='by_username'),
     path('last_interaction/user/<id>', last_interacted, name='last_activity')
