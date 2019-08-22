060bab6ffd5244c858b6307ecdbb6fc355d2339e
Sebastian Oliva <code@sebastianoliva.com>
2019-08-22 15:56:52 -0600

    machine

--- messenger_users/models.py
+++ messenger_users/models.py
@@ -1,5 +1,8 @@
 import uuid
 from django.db import models
+from transitions import Machine
+from django.db.models.signals import post_init
+from django.dispatch import receiver
 
 
 class User(models.Model):
@@ -66,7 +69,55 @@ class Referral(models.Model):
 
 
 class UserActivity(models.Model):
+
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
     class Meta:
         app_label = 'messenger_users'
+
+    WAIT = 'wait'
+    USER_QUERY = 'user_query'
+    BROADCAST_START= 'broadcast_start'
+    TIMED_START = 'timed_start'
+    ACTIVE_SESSION = 'active_session'
+    DISPATCHED = 'dispatched'
+    OPENED = 'opened'
+    FOLLOW_UP = 'follow_up'
+
+    STATE_TYPES = [
+        (WAIT, WAIT),
+        (USER_QUERY, USER_QUERY),
+        (BROADCAST_START, BROADCAST_START),
+        (TIMED_START, TIMED_START),
+        (ACTIVE_SESSION, ACTIVE_SESSION),
+        (DISPATCHED, DISPATCHED),
+        (OPENED, OPENED),
+        (FOLLOW_UP, FOLLOW_UP),
+    ]
+
+    state = models.CharField(
+        "state",
+        max_length=100,
+        choices=STATE_TYPES,
+        default=WAIT,
+        help_text='stado',
+    )
+
+
+@receiver(post_init, sender=UserActivity)
+def init_state_machine(instance, **kwargs):
+    states = [state for state, _ in instance.STATE_TYPES]
+    machine = instance.machine = Machine(model=instance, states=states, initial=instance.WAIT, \
+                                         ignore_invalid_triggers=True)
+    machine.add_transition('receive_user_message', instance.WAIT, instance.USER_QUERY)
+    machine.add_transition('send_broadcast', instance.WAIT, instance.BROADCAST_START)
+    machine.add_transition('awaited_enough', instance.WAIT, instance.TIMED_START)
+    machine.add_transition('want_activity', instance.USER_QUERY, instance.ACTIVE_SESSION)
+    machine.add_transition('want_activity', instance.BROADCAST_START, instance.ACTIVE_SESSION)
+    machine.add_transition('want_activity', instance.TIMED_START, instance.ACTIVE_SESSION)
+    machine.add_transition('get_post', instance.ACTIVE_SESSION, instance.DISPATCHED)
+    machine.add_transition('open_post', instance.DISPATCHED, instance.OPENED)
+    machine.add_transition('give_feedback', instance.OPENED, instance.FOLLOW_UP)
+    machine.add_transition('give_feedback', instance.FOLLOW_UP, instance.FOLLOW_UP)
+    machine.add_transition('end_feedback', instance.FOLLOW_UP, instance.WAIT)
+    machine.add_transition('no_feedback', instance.OPENED, instance.WAIT)
