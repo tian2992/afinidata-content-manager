7cccd1c5cb4290cde7cde0f0d208173f29d3d0e8
Boris Aguilar <baguilar@innovare.es>
2020-03-06 10:42:18 +0400

    Adding validation for future dates

--- utilities/views.py
+++ utilities/views.py
@@ -217,6 +217,19 @@ def validates_kids_date(request):
                                 messages=[]
                             )
                         )
+                    #also validate if date is future (not just next year but next months)
+                    date = parse(request.GET['date'], dayfirst=True)
+                    rel = relativedelta.relativedelta(datetime.now(), date)
+                    child_months = (rel.years * 12) + rel.months
+                    if child_months < 0:
+                        return JsonResponse(
+                            dict(
+                                set_attributes=dict(
+                                    isDateValid=False
+                                ),
+                                messages=[]
+                            )
+                        )
 
                 if second_result:
                     print('second here!')
