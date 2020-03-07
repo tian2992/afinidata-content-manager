e09bc2d1aaad74f7f7809563fa9c1dc55fa0da4c
Sebastian Oliva <code@sebastianoliva.com>
2020-03-07 08:50:01 -0600

    add question response admin

--- posts/admin.py
+++ posts/admin.py
@@ -3,6 +3,8 @@ from posts import models
 
 admin.site.register(models.Post)
 admin.site.register(models.Question)
-admin.site.register(models.Response)
+admin.site.register(models.QuestionResponse)
 admin.site.register(models.Interaction)
 admin.site.register(models.PostLocale)
+
+admin.site.register(models.Response)
