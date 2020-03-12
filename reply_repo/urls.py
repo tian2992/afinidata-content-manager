e6ded45b3295e3a2f59e466799f4035238a579a4
Sebastian Oliva <code@sebastianoliva.com>
2020-03-12 16:06:14 -0600

    fix messages

--- reply_repo/urls.py
+++ reply_repo/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from reply_repo.views import index, download_messages, upload_messages, fix_messages_view
 
-app_name = 'dash'
+app_name = 'reply_repo'
 
 urlpatterns = [
     path('', index, name="index"),
-    path('fix_messages', fix_messages_view, name='fixer'),
-    path('download', download_messages, name="down"),
-    path('upload', upload_messages, name="up")
+    path('fix_messages/', fix_messages_view, name='fixer'),
+    path('fix_messages/download', download_messages, name="down"),
+    path('fix_messages/upload', upload_messages, name="up")
 ]
